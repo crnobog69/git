222ec2ea8d6160de002e6b6614323224dbb4b3b4
crnobog <carskalavra@proton.me>
2024-10-19 14:11:17 +0200

    ❄️

---
+++ git.ps1
@@ -0,0 +1,22 @@
+echo "# Git" >> README.md
+git init
+git add README.md
+git add .
+git commit -m "❄️"
+git branch -M main
+
+# -------
+
+git remote add origin git@github.com:crnobog69/git.git
+
+# git remote add codeberg 
+
+# git remote add gitlab
+
+# -------
+
+git push origin main
+
+# git push codeberg main
+
+# git push gitlab main
